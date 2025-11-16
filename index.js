5ae7d1e60571b509f95001d708ce0516cb4c9205
GOKUL Selvam <gokulsece2022@ksrce.ac.in>
2025-11-16 17:27:02 +0530

    CI/CD Pipeline project

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello !"));
-app.get("/", (req, res) => res.send("I'm Gokul"));
-app.get("/", (req, res) => res.send("AWS Certified Cloud Practitioner"));
-app.get("/", (req, res) => res.send("This is my static website created for the Intern task"));
+app.get("/", (req, res) => {
+  res.send(`
+    Hello! <br>
+    I'm Gokul <br>
+    AWS Certified Cloud Practitioner <br>
+    This is my static website created for the Intern task
+  `);
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
