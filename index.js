7ff73875e53d8a0b8e2613e8172ccee0161eeaf0
GOKUL Selvam <gokulsece2022@ksrce.ac.in>
2025-11-16 17:09:48 +0530

    Tiny change

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello from CI/CD Pipeline!"));
+app.get("/", (req, res) => res.send("Hello !"));
+app.get("/", (req, res) => res.send("I'm Gokul"));
+app.get("/", (req, res) => res.send("AWS Certified Cloud Practitioner"));
+app.get("/", (req, res) => res.send("This is my static website created for the Intern task"));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
